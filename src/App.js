f98a77b28ca77249d651527ca70cd487c7cf792b
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2023-12-31 01:15:01 +0600

    Updating

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css"
 function App() {
   return (
     <div className="App">
-        <div className="fade"></div>
-        <div className="fade"></div>
+        <div className="fade" style={{top: '-6%', right:'0'}}></div>
+        <div className="fade" style={{top: '34%', left:"-6rem"}}></div>
     </div>
   );
 }
