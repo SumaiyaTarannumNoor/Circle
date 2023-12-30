dcc4eaf491fd532e9a9e29f44266115d0e7634c2
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2023-12-31 01:17:02 +0600

    Updating...

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
         <div className="fade" style={{top: '-6%', right:'0'}}></div>
-        <div className="fade" style={{top: '34%', left:"-6rem"}}></div>
+        <div className="fade" style={{top: '34%', left: '-6rem'}}></div>
     </div>
   );
 }
