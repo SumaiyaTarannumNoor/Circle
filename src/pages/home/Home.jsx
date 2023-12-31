980f11b2aaf288b1300be63700a46e22323cc726
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2023-12-31 21:12:43 +0600

    Adding...

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const home = () => {
+  return (
+    <div className="Home">
+        <div className="Profile"></div>
+        <div className="Post"></div>
+        <div className="RightSide"></div>
+    </div>
+  )
+}
+
+export default home
