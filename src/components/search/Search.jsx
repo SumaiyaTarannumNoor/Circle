2c37138584a999e39644a4ecc6e1a76bd87cae32
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-01-02 10:24:09 +0600

    Adding and Updating...

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Logo from '../../img/logo.png'
+import './Search.css'
 
 const Search = () => {
   return (
     <div className="Search">
-        CrACKHeAD
+        <img src={Logo} alt="" id='Logo'/>
     </div>
   )
 }
