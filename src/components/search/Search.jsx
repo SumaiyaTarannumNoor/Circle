b5fa1880201759e21ba8934dc73e01dd680d58ac
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-01-02 12:24:33 +0600

    Updating...

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Logo from '../../img/logo.png'
 import './Search.css'
+import {UilSearch} from '@iconscout/react-unicons'
 
 const Search = () => {
   return (
     <div className="Search">
         <img src={Logo} alt="" id='Logo'/>
+        <div className="SearchBar">
+          <input type="text" placeholder='ExploreMore' id='Bar' />
+          <div className="SearchIcon">
+            <UilSearch/>
+          </div>
+        </div>
     </div>
   )
 }
