c1ca5268427f5d69d6de04cc5fa630c2a69b93be
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-01-07 21:29:36 +0600

    Updating

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './PostShare.css'
+import ProfileImage from '../../img/img1.jpg'
+
+import {UilScenery} from "@iconscout/react-unicons"
+import {UilPlayCircle} from "@iconscout/react-unicons"
+import {UilLocationPoint} from "@iconscout/react-unicons"
+import {UilSchedule} from "@iconscout/react-unicons"
 
 const PostShare = () => {
   return (
-    <div>PostShare</div>
+    <div className="PostShare">
+        <img src= {ProfileImage} alt="" />
+        <div>
+            <input type="text" placeholder='Wanna say something???' />
+            <div className="PostOptions">
+                <div className="Options">
+                    <UilScenery/>
+                    Photo
+                </div>
+                <div className="Options">
+                    <UilPlayCircle/>
+                    Video
+                </div><div className="Options">
+                    <UilLocationPoint/>
+                    Location
+                </div><div className="Options">
+                    <UilSchedule/>
+                    Schedule
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
