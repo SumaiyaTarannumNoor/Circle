cc572984bad65de674134a531c5f6ec20ce6d83a
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-02-25 22:19:04 +0600

    Because I weant to...

--- src/components/ProfileModal/ProfileModal.jsx
+++ src/components/ProfileModal/ProfileModal.jsx
@@ -1,5 +1,5 @@
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button } from '@mantine/core';
+import { Modal, Button, ColorSchemeProvider } from '@mantine/core';
 
 function ProfileModal({modalOpened, setModalOpened}) {
   const [opened, { open, close }] = useDisclosure(false);
@@ -10,13 +10,23 @@ function ProfileModal({modalOpened, setModalOpened}) {
         opened={modalOpened}
         onClose={()=>setModalOpened(false)}
         title="Authentication"
+        overlayColor={
+          ColorSchemeProvider === ""
+        }
         overlayProps={{
           backgroundOpacity: 0.55,
-          blur: 3,
+          blur: 4,
+          size: 60
         }}
       >
         <form action="" className="InfoForm">
             <h3>Your info that you have sold already...</h3>
+
+            <div>
+              <input type="text" className='InfoInput' name="FirstName" placeholder='First Name' />
+              
+              <input type="text" className='InfoInput' name="LastName" placeholder='Last Name' />
+            </div>
         </form>
       </Modal>
     </>
