513f105035c292440f1ac64c89bc8e36788d76fb
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-03-09 00:41:47 +0600

    Updated.

--- Server/index.js
+++ Server/index.js
@@ -1,3 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+
+const app = express();
+app.use(bodyParser.json({limit: '40mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '40mb', extended: true}))
