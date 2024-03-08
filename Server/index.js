003bcf7a5b6f103e8d49725087823f1854ae7768
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-03-09 01:20:10 +0600

    Updated.

--- Server/index.js
+++ Server/index.js
@@ -7,5 +7,6 @@ app.use(bodyParser.json({ limit: '40mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '40mb', extended: true }))
 
 mongoose
-    .connect("mongodb+srv://sumaiyatarannumnoor001:CIRCLEcircleCircle@cluster0.cdxraz7.mongodb.net/Circle?retryWrites=true&w=majority&appName=Cluster0")
+    .connect("mongodb+srv://sumaiyatarannumnoor001:CIRCLEcircleCircle@cluster0.cdxraz7.mongodb.net/Circle?retryWrites=true&w=majority&appName=Cluster0", 
+    {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(6000, () => console.log("Running the Destruction...")))
