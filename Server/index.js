4ba55f863498e1e57c9666ffdc749e9fcd29ac68
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-03-09 01:47:52 +0600

    Updated.

--- Server/index.js
+++ Server/index.js
@@ -13,3 +13,4 @@ mongoose
     .connect(process.env.MONGO_DB, 
     {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(process.env.PORT, () => console.log(`Running the Destruction at ${process.env.PORT}...`)))
+    .catch((error) => console.log(`Unable to run the Destruction at ${process.env.PORT}`));
