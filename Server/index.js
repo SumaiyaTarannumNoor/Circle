feaa9fdf9afb08c6f9481d432f01e04a4a3b5388
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-03-09 01:10:31 +0600

    Updated

--- Server/index.js
+++ Server/index.js
@@ -5,3 +5,5 @@ import mongoose from 'mongoose';
 const app = express();
 app.use(bodyParser.json({limit: '40mb', extended: true}))
 app.use(bodyParser.urlencoded({limit: '40mb', extended: true}))
+
+mongoose.connect("mongodb+srv://sumaiyatarannumnoor001:CIRCLEcircleCircle@cluster0.cdxraz7.mongodb.net/Circle?retryWrites=true&w=majority&appName=Cluster0")
