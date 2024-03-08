efe1840baa8dbb17990cbfa8f5c3607ad91f800d
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-03-09 01:16:25 +0600

    Updated.

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
 const app = express();
-app.use(bodyParser.json({limit: '40mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '40mb', extended: true}))
+app.use(bodyParser.json({ limit: '40mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '40mb', extended: true }))
 
-mongoose.connect("mongodb+srv://sumaiyatarannumnoor001:CIRCLEcircleCircle@cluster0.cdxraz7.mongodb.net/Circle?retryWrites=true&w=majority&appName=Cluster0")
+mongoose
+    .connect("mongodb+srv://sumaiyatarannumnoor001:CIRCLEcircleCircle@cluster0.cdxraz7.mongodb.net/Circle?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => app.listen(6000, () => console.log("Running the Destruction...")))
