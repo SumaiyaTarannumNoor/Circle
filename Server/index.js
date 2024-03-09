c107bf5ec73d0477458e62dc90c47d6e859304eb
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-03-09 10:50:57 +0600

    Updated.

--- Server/index.js
+++ Server/index.js
@@ -2,8 +2,14 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import { configDotenv } from 'dotenv';
+import AuthRoute from './Routes/AuthRoute.js'
 
+//Rotes
 const app = express();
+
+
+
+//Middleware
 app.use(bodyParser.json({ limit: '40mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '40mb', extended: true }))
 
@@ -14,3 +20,7 @@ mongoose
     {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(process.env.PORT, () => console.log(`Running the Destruction at ${process.env.PORT}...`)))
     .catch((error) => console.log(`Unable to run the Destruction at ${process.env.PORT}`));
+
+
+//usage of routes 
+app.use('/auth', AuthRoute)   
