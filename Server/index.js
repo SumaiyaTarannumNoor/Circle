8aa7fb34f8556e8479483ae6af816961841cf37b
SumaiyaTarannumNoor <sumaiyatarannumnoor001@gmail.com>
2024-04-06 14:14:01 +0600

    Now it's running 😑

--- Server/index.js
+++ Server/index.js
@@ -15,12 +15,16 @@ app.use(bodyParser.urlencoded({ limit: '40mb', extended: true }))
 
 configDotenv()
 
+// mongoose
+//     .connect(process.env.MONGO_DB, 
+//     {useNewUrlParser: false, useUnifiedTopology: true})
+//     .then(() => app.listen(process.env.PORT, () => console.log(`Running the Destruction at ${process.env.PORT}...`)))
+//     .catch((error) => console.log(`Unable to run the Destruction at ${process.env.PORT}`));
+
 mongoose
     .connect(process.env.MONGO_DB)
-    // {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(process.env.PORT, () => console.log(`Running the Destruction at ${process.env.PORT}...`)))
-    // .catch((error) => console.log(`Unable to run the Destruction at ${process.env.PORT}`));
+    .catch((error) => console.log(`Unable to run the Destruction at ${process.env.PORT}`, error)); // Include error parameter
 
-
-//usage of routes 
-app.use('/auth', AuthRoute)   
+// Usage of routes
+app.use('/auth', AuthRoute);  
