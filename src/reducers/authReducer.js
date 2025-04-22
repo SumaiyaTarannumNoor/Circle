c66712bbe777097ac2dbfa93313bcc93be32f8e6
Sumaiya Tarannum Noor <sumaiyatarannumnoor001@gmail.com>
2025-04-22 21:27:31 +0600

    Updating Statig Again...

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { act } from "react";
+
 const authReducer = (
     state = {authData: null, loading: false, error:false},
     action
@@ -7,11 +9,11 @@ const authReducer = (
             return {...state, loading: true, error: false};
         case "AUTH_SUCCESS":
             localStorage.setItem("profile", JSON.stringify({...action?.data}));
-            return {...state, authData: action.data, loading: false, error: false};
-        case "AUTH_FAIL": 
-            return {...state, loading: false, error: true} 
-        default: 
-            return state;     
+            return {...state, authData:action.data, loading: false} 
+        case "AUTH_FAIL":
+            return {...state, loading:false, error:true};
+        default:
+            return state;             
     }
 };
 
